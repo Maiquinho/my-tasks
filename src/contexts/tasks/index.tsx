efc8be396e20554b7f21e0130653c188df821a4a
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-15 15:06:49 -0300

    build: add tasks context

---
+++ src/contexts/tasks/index.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, createContext, useReducer } from "react"
+import { tasksInitialState, tasksReducer } from "../../hooks/reducers/taskReducer";
+import { ReducerActionType } from "../../types/settingsReducer";
+import { Tasks } from "../../types/task";
+
+
+interface TasksContextInitialStateType {
+    tasks: Tasks
+}
+
+interface TasksContextType {
+    state: TasksContextInitialStateType,
+    dispatch: React.Dispatch<any>
+}
+
+
+const initialTasksContext = {
+    tasks: tasksInitialState
+}
+
+const TasksContext = createContext<TasksContextType>({
+    state: initialTasksContext,
+    dispatch: () => null
+});
+
+const mainReducer = (state: TasksContextInitialStateType, action: ReducerActionType) => ({
+    tasks: tasksReducer(state.tasks, action)
+});
+
+const TasksProvider: React.FC<PropsWithChildren> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(mainReducer, initialTasksContext);
+
+    return (
+        <TasksContext.Provider value={{ state, dispatch }}>
+            { children }
+        </TasksContext.Provider>
+    )
+}
+
+
+export { TasksContext, TasksProvider }
