322d7f2edba39551c0d4a53a904915cecb30b653
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-16 13:37:56 -0300

    build: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer(){
+    return (
+        <footer className="flex-1 flex flex-col justify-end p-4 lg:pb-[60px] mt-2 text-center">
+            <span className="text-xs dark:text-gray-600 text-gray-800">Made with ❤️ by: <a href="https://github.com/maiquinho">Maiquinho</a></span>
+        </footer>
+    )
+}
