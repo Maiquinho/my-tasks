a2bebd0319af300beffac141ad2a25126e7f8193
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-16 13:11:56 -0300

    refactor: divide App component from logic using custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Tasks } from './components/Tasks';
 import { UserSettings } from "./components/UserSettings";
+import { useSettingsModal } from './hooks/settings/settingsModal';
 
 import './styles/global.css';
 
 
 export function App() {
 
-  const [displayModalConfig, setDisplayModalConfig] = useState(false);
-
-  function handleUserConfigs() {
-    if (displayModalConfig == false) {
-      setDisplayModalConfig(true)
-    } else {
-      setDisplayModalConfig(false)
-    }
-  }
-
+  const { handleUserConfigs, displayModalConfig } = useSettingsModal();
 
   return (
-    <div className="relative flex justify-center h-screen">
-      <div className="w-[80%] mx-auto">
-        <Header userConfigs={handleUserConfigs} />
-        <Tasks />
+    <>
+      <div className="relative dark:bg-gray-800 flex justify-center h-screen">
+        <div className="w-[80%] mx-auto flex flex-col">
+          <Header userConfigs={handleUserConfigs} />
+          <Tasks />
+          <Footer />
+        </div>
+
+        <UserSettings
+          displayModalConfig={displayModalConfig}
+          handleModalDisplay={handleUserConfigs}
+        />
       </div>
-      
-      <UserSettings 
-        displayModalConfig={displayModalConfig} 
-        handleModalDisplay={handleUserConfigs} 
-      />
-    </div>
+    </>
   )
 }
