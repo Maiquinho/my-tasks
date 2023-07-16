f1e994bc117a6b6949444e987517a5c515adca3d
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-16 13:36:13 -0300

    build(ConfigButton): add darkmode css classNames

--- src/components/ConfigButton.tsx
+++ src/components/ConfigButton.tsx
@@ -9,7 +9,7 @@ export function ConfigButton({clickFn}: configButtonProps) {
 
     return (
         <button className="h-fit" onClick={clickFn}>
-            <Gear width={28} height={28} className="text-gray-800 hover:rotate-180 hover:text-red transition-all" />
+            <Gear width={28} height={28} className="dark:text-gray-200 text-gray-800 hover:rotate-180 dark:hover:text-red hover:text-red transition-all" />
         </button>
     )
 }
