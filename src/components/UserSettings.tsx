cd055d06b5fb9f3fa39ff245d9544782595e603d
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-16 13:47:16 -0300

    build: add darkmode classNames & set new z-index position

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -7,14 +7,15 @@ type UserSettingsProps = {
 
 
 export function UserSettings({ displayModalConfig, handleModalDisplay }: UserSettingsProps) {
+    
 
     return (
         <>
             {displayModalConfig ?
                 <>
-                    <div className="absolute bg-gray-800 z-40 h-screen w-full inset-0 opacity-60 visible transition-opacity" onClick={handleModalDisplay}></div>
-                    <div className="absolute bg-gray-200 z-50 bottom-4 h-fit w-11/12 p-6 opacity-100 visible transition-opacity">
-                        <h2 className="font-semi-bold mb-4">Configurações</h2>
+                    <div className="absolute dark:opacity-40 bg-gray-800 z-[50] h-screen w-full inset-0 opacity-60 visible transition-opacity" onClick={handleModalDisplay}></div>
+                    <div className="absolute dark:bg-gray-900 bg-gray-200 z-[100] bottom-4 h-fit w-11/12 p-6 opacity-100 visible transition-opacity rounded">
+                        <h2 className="dark:text-gray-200 text-gray-800 font-semi-bold mb-4">Configurações</h2>
 
                         <DarkModeButton />
                     </div>
@@ -23,8 +24,10 @@ export function UserSettings({ displayModalConfig, handleModalDisplay }: UserSet
                 :
 
                 <>
-                    <div className="absolute bg-gray-800 z-40 h-screen w-full inset-0 opacity-0 invisible transition-opacity"></div>
-                    <div className="absolute z-50 opacity-0 invisible transition-opacity"></div>
+                    <div className="absolute dark:opacity-40 bg-gray-800 z-[50] h-screen w-full inset-0 opacity-0 invisible transition-opacity"></div>
+                    <div className="absolute z-[100] opacity-0 invisible transition-opacity">
+                        <DarkModeButton />
+                    </div>
                 </>
             }
         </>
