a26b6c840430eaf6eb741f78721d6371c36e8f02
Michael Matheus <michaelmatheus2001@gmail.com>
2023-07-20 11:09:27 -0300

    fix: change modal config position to absolute

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -14,7 +14,7 @@ export function UserSettings({ displayModalConfig, handleModalDisplay }: UserSet
             {displayModalConfig ?
                 <>
                     <div className="absolute dark:opacity-40 bg-gray-800 z-[50] h-screen w-full inset-0 opacity-60 visible transition-opacity" onClick={handleModalDisplay}></div>
-                    <div className="absolute dark:bg-gray-900 bg-gray-200 z-[100] bottom-4 h-fit w-11/12 p-6 opacity-100 visible transition-opacity rounded">
+                    <div className="fixed dark:bg-gray-900 bg-gray-200 z-[100] bottom-4 h-fit w-11/12 p-6 opacity-100 visible transition-opacity rounded">
                         <h2 className="dark:text-gray-200 text-gray-800 font-semi-bold mb-4">Configurações</h2>
 
                         <DarkModeButton />
@@ -25,7 +25,7 @@ export function UserSettings({ displayModalConfig, handleModalDisplay }: UserSet
 
                 <>
                     <div className="absolute dark:opacity-40 bg-gray-800 z-[50] h-screen w-full inset-0 opacity-0 invisible transition-opacity"></div>
-                    <div className="absolute z-[100] opacity-0 invisible transition-opacity">
+                    <div className="fixed z-[100] opacity-0 invisible transition-opacity">
                         <DarkModeButton />
                     </div>
                 </>
